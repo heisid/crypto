d464df63c56137417f99df0c92ac77b1ea626b0d
heisid <weird_alien@protonmail.com>
2019-12-09 01:02:09 +0700

    isprime

--- tools.py
+++ tools.py
@@ -1,7 +1,7 @@
 import math
 import sys
 
-def prime_factors(n):
+def factors(n):
     result = []
     while n % 2 == 0:
         result.append(2)
@@ -13,14 +13,23 @@ def prime_factors(n):
             n = n / i
 
     if n > 2:
-        result.append(n)
+        result.append(int(n))
 
     return result
+
+def isprime(n):
+   result = False
+   if n and (n != 1):
+       allfactors = factors(n)
+       if len(allfactors) == 1:
+           result = True
+   return result
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
-        print(prime_factors(int(sys.argv[1])))
+        # print(factors(int(sys.argv[1])))
+        print(isprime(int(sys.argv[1])))
     else:
         print("argument invalid")
 
