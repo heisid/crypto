b2f125836ae037efe62c9c3a6c317c2b2e7c7ffc
heisid <weird_alien@protonmail.com>
2019-12-28 13:59:28 +0700

    oop because it's cool

--- affine.py
+++ affine.py
@@ -5,40 +5,36 @@
 import sys
 import tools 
 
-def generate_possible_a_keys(mod):
-   a_keys = list()
-   for element in range(mod):
-      if tools.are_coprime(element, mod):
-         a_keys.append(element)
-   return a_keys
 
-def encrypt(plain_list, a_key, b_key, mod):
-   if not tools.are_coprime(a_key, mod):
-      raise tools.NotCoprimeError
-   if b_key > mod:
-      raise "Invalid b key"
-   cipher_list = [((a_key*char + b_key) % mod) for char in plain_list]
-   return cipher_list
+class Affine:
+   def __init__(self, a_key=1, b_key=1, mod=107):
+      self.a_key = a_key
+      self.b_key = b_key
+      self.mod = mod
 
-def decrypt(cipher_list, a_key, b_key, mod):
-   a_inverse = tools.mod_inverse(a_key,mod)
-   plain_list = [((a_inverse * (char - b_key)) % mod) for char in cipher_list]
-   return plain_list
+   def generate_possible_a_keys(self):
+      return [element for element in range(self.mod) if tools.are_coprime(element, self.mod)]
 
-def encrypt_text(plaintext, a_key, b_key):
-   # printable ascii range 20 - 126
-   # so we need 126 - 20 + 1 = mod 107
-   # 107 is prime, don't worry about inverse existence
-   mod = 107
-   plaintext = tools.string_to_numcode(plaintext)
-   ciphertext = encrypt(plaintext, a_key, b_key, mod)
-   return tools.numcode_to_string(ciphertext)
+   def __encrypt(self, plain_list):
+      if not tools.are_coprime(self.a_key, self.mod):
+         raise tools.NotCoprimeError
+      if self.b_key > self.mod:
+         raise "Invalid b key"
+      return [((self.a_key*char + self.b_key) % self.mod) for char in plain_list]
 
-def decrypt_text(ciphertext, a_key, b_key):
-   ciphertext = tools.string_to_numcode(ciphertext)
-   mod = 107
-   plaintext = decrypt(ciphertext, a_key, b_key, mod)
-   return tools.numcode_to_string(plaintext)
+   def __decrypt(self, cipher_list):
+      a_inverse = tools.mod_inverse(self.a_key, self.mod)
+      return [((a_inverse * (char - self.b_key)) % self.mod) for char in cipher_list]
+
+   def encrypt_text(self, plaintext):
+      plaintext = tools.string_to_numcode(plaintext)
+      ciphertext = self.__encrypt(plaintext)
+      return tools.numcode_to_string(ciphertext)
+
+   def decrypt_text(self, ciphertext):
+      ciphertext = tools.string_to_numcode(ciphertext)
+      plaintext = self.__decrypt(ciphertext)
+      return tools.numcode_to_string(plaintext)
 
 # TODO: file or stdin inputs
 # TODO: encrypt/decrypt binary
@@ -46,12 +42,11 @@ def decrypt_text(ciphertext, a_key, b_key):
 if __name__ == "__main__":
    import argparse
    
-   FUNCTION_MAP = {"encrypt": encrypt_text, "decrypt": decrypt_text}
+   affine = Affine()
+   FUNCTION_MAP = {"encrypt": affine.encrypt_text, "decrypt": affine.decrypt_text}
 
    parser = argparse.ArgumentParser()
 
-   # group = parser.add_mutually_exclusive_group(required=True)
-   # group.add_argument("func", choices=FUNCTION_MAP.keys())
    parser.add_argument("func", choices=FUNCTION_MAP.keys())
 
    parser.add_argument("-a", "--a_key", type=int, required=True, help="'a' key of affine cipher (integer 0-106)")
@@ -62,14 +57,14 @@ if __name__ == "__main__":
    args = parser.parse_args()
 
    func = FUNCTION_MAP[args.func]
-   a_key, b_key = args.a_key, args.b_key
+   affine.a_key, affine.b_key = args.a_key, args.b_key
    infile = args.input_file
    outfile = args.output_file
    filebuffer = list()
 
    with open(infile, "r") as reader:
       for line in reader:
-         processed_line = func(line.rstrip('\n'), a_key, b_key)
+         processed_line = func(line.rstrip('\n'))
          print(processed_line)
          filebuffer.append(processed_line + '\n')
 
